10e50dd88b536e327641e2c459729c4018a2bd5b
aweoluwatobi <aweoluwatobi26@gmail.com>
2023-11-15 18:24:05 +0100

    Updated file to use latest ArcGIS Maps SDK for javascript version syntax

--- script.js
+++ script.js
@@ -3,34 +3,46 @@ let view;
 let map;
 let mapLayer;
 let layer;
-let basemaps = [] //create empty basemap array
+let basemaps = []; //create empty basemap array
 
 // Connect to ESRI Javascript API and get necessary objects
-require(["esri/Map", "esri/views/MapView", "esri/request", "esri/layers/MapImageLayer", "esri/widgets/Legend"], function (Map, MapView, Request, MapLayer, Legend) {
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/request",
+  "esri/layers/MapImageLayer",
+  "esri/widgets/Legend",
+], function (Map, MapView, Request, MapLayer, Legend) {
   // create new map
-  map = new Map({basemap:'streets'});
+  map = new Map({ basemap: "streets" });
   // create options for the map view
-  let viewOptions = {container:'mapview', map:map, center:[-117.11091686122334, 32.70527463861], scale:10000};
+  let viewOptions = {
+    container: "mapview",
+    map: map,
+    center: [-117.11091686122334, 32.70527463861],
+    scale: 10000,
+  };
   // create map view
   view = new MapView(viewOptions);
   // create legend after view loads
-  view.then(() => {
-    let legend = new Legend({view: view});
+  view.when(() => {
+    let legend = new Legend({ view: view });
     view.ui.add(legend, "bottom-left");
-  })
-        
+  });
+
   // Connect to ArcGIS Server sample URL
   // let url = 'https://server.arcgisonline.com/arcgis/rest/services/reference?f=json'
-        
-  let url = 'https://sampleserver6.arcgisonline.com/arcgis/rest/services?f=json'
+
+  let url =
+    "https://sampleserver6.arcgisonline.com/arcgis/rest/services?f=json";
 
   // Set request option to indicate json as response type
-  let options = {responseType: "json"};
-        
-  //Request json data specifying the url and options
-  Request(url, options).then(addMapServices)
+  let options = { responseType: "json" };
 
-  function addMapServices (response) {
+  //Request json data specifying the url and options
+  Request(url, options).then(addMapServices);
+
+  function addMapServices(response) {
     generateBasemaps();
     let result = response.data;
     // Get services div created in HTML
@@ -38,7 +50,7 @@ require(["esri/Map", "esri/views/MapView", "esri/request", "esri/layers/MapImage
 
     // Add services to a select dropdown list
     for (let i = 0; i < result.services.length; i++) {
-      // Create options 
+      // Create options
       let option = document.createElement("option");
       // Set options text content to the name of services
       option.textContent = result.services[i].name;
@@ -51,90 +63,88 @@ require(["esri/Map", "esri/views/MapView", "esri/request", "esri/layers/MapImage
   lstServices.addEventListener("change", changeMapService);
 
   function changeMapService() {
-        // Access services using the selectedIndex option of that service
-        let selectedService = lstServices[lstServices.selectedIndex].textContent;
-        // Create and access Map layer from services url using services name selected from dropdown
-        layer = new MapLayer({url: `https://sampleserver6.arcgisonline.com/arcgis/rest/services/${selectedService}/MapServer`})
-        //Make sure any layer on map is removed to avoid multiple layers added from previous change event
-        map.removeAll();
-        // Add Layer to map
-        map.add(layer);
-              // When layer loads, go to full extent of the layer
-        layer.then(() => {
-          view.goTo(layer.fullExtent)
-  
-          // Get Table of Content Element
-          let toc = document.getElementById("toc");
-          toc.innerHTML = "";
-          let layerList = document.createElement("ul");
-          
-          addLayerToContent(layer, layerList)
-  
-        });
-              
-      }
-             
+    // Access services using the selectedIndex option of that service
+    let selectedService = lstServices[lstServices.selectedIndex].textContent;
+    // Create and access Map layer from services url using services name selected from dropdown
+    layer = new MapLayer({
+      url: `https://sampleserver6.arcgisonline.com/arcgis/rest/services/${selectedService}/MapServer`,
+    });
+    //Make sure any layer on map is removed to avoid multiple layers added from previous change event
+    map.removeAll();
+    // Add Layer to map
+    map.add(layer);
+    // When layer loads, go to full extent of the layer
+    layer.when(() => {
+      view.goTo(layer.fullExtent);
+
+      // Get Table of Content Element
+      let toc = document.getElementById("toc");
+      toc.innerHTML = "";
+      let layerList = document.createElement("ul");
+
+      addLayerToContent(layer, layerList);
+    });
+  }
 });
 
 // Generate list of buttons to show various basemaps
-function generateBasemaps () {
+function generateBasemaps() {
   // Push basemaps to Array
-  basemaps.push('satellite'); // Add satellite basemap
-  basemaps.push('topo') // Add topo basemap
-  basemaps.push('osm') // Add osm basemap
-  basemaps.push('hybrid') // Add hybrid basemap
-  basemaps.push('terrain') // Add terrain basemap
-  basemaps.push('dark-gray') // Add dark-gray basemap
-  basemaps.push('national-geographic') // Add national-geographic basemap
-  basemaps.push('oceans') // Add oceans basemap
-  basemaps.push('gray')
-  basemaps.push('dark-gray')
+  basemaps.push("satellite"); // Add satellite basemap
+  basemaps.push("topo"); // Add topo basemap
+  basemaps.push("osm"); // Add osm basemap
+  basemaps.push("hybrid"); // Add hybrid basemap
+  basemaps.push("terrain"); // Add terrain basemap
+  basemaps.push("dark-gray"); // Add dark-gray basemap
+  basemaps.push("national-geographic"); // Add national-geographic basemap
+  basemaps.push("oceans"); // Add oceans basemap
+  basemaps.push("gray");
+  basemaps.push("dark-gray");
 
   // set basemap to the text content of each target
-  let setBasemap = e => map.basemap = e.target.textContent;
+  let setBasemap = (e) => (map.basemap = e.target.textContent);
 
   // create basemap buttons
-  for (let i=0; i < basemaps.length; i++) {
+  for (let i = 0; i < basemaps.length; i++) {
     //Get basemap buttons div created in HTML
-    let basemapBtns = document.getElementById('basemap-btns');
+    let basemapBtns = document.getElementById("basemap-btns");
     // Create basemap button
-    let basemapBtn = document.createElement('button');
+    let basemapBtn = document.createElement("button");
     // Set basemap button ID
     basemapBtn.id = basemaps[i];
     // Set basemap button text content
     basemapBtn.textContent = basemaps[i];
     // add event listener on click to set basemap
-    basemapBtn.addEventListener('click', setBasemap)
+    basemapBtn.addEventListener("click", setBasemap);
     // add basemap buttons to basemap button div created in HTML
-    basemapBtns.append(basemapBtn)
+    basemapBtns.append(basemapBtn);
   }
 }
 
-function addLayerToContent (maplayer, layerList) {
+function addLayerToContent(maplayer, layerList) {
   for (i = 0; i < maplayer.sublayers.length; i++) {
-    let subLayer = maplayer.findSublayerById(i)
+    let subLayer = maplayer.findSublayerById(i);
 
     let layerItem = document.createElement("li");
-    let layerInput = document.createElement("input")
-    layerInput.setAttribute("type", "checkbox")
-    layerInput.setAttribute("id", subLayer.title)
-    layerInput.value = subLayer.id
+    let layerInput = document.createElement("input");
+    layerInput.setAttribute("type", "checkbox");
+    layerInput.setAttribute("id", subLayer.title);
+    layerInput.value = subLayer.id;
 
-    let layerLabel = document.createElement("label")
-    layerLabel.textContent = subLayer.title
-    layerLabel.setAttribute("for", subLayer.title)
+    let layerLabel = document.createElement("label");
+    layerLabel.textContent = subLayer.title;
+    layerLabel.setAttribute("for", subLayer.title);
     layerInput.checked = subLayer.visible;
-        
-    layerItem.append(layerInput)
-    layerItem.append(layerLabel)
 
-    layerList.append(layerItem)
-    toc.append(layerList)
-          
+    layerItem.append(layerInput);
+    layerItem.append(layerLabel);
+
+    layerList.append(layerItem);
+    toc.append(layerList);
+
     layerInput.addEventListener("change", (e) => {
-      let checkedLayer = maplayer.findSublayerById(Number(e.target.value))
+      let checkedLayer = maplayer.findSublayerById(Number(e.target.value));
       checkedLayer.visible = e.target.checked;
-              
-    })
+    });
   }
 }
